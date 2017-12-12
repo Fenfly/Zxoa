a001ecbe3bf117dde0f4a7ca9838902a94a32536
lzxcstf <cstflzx@163.com>
2017-12-12 17:29:01 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Zxoa
+# Zxoa
---
+++ Zxoa/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Zxoa
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
